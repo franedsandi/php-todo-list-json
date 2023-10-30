e18bb30055a2a7a7fc1e35a921af3b65c8c1b145
franedsandi <franedsandi@gmail.com>
2023-10-30 17:28:34 +0100

    innit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once __DIR__ . '/funtions.php';
+require_once __DIR__ . '/partials/head.php';
+
+?>
+<body>
+  <div class="container">
+    <h1>to do list</h1>
+  </div>
+</body>
---
+++ server.php
@@ -0,0 +1,6 @@
+<?php
+
+$json_string = file_get_contents('todo-list.json');
+var_dump($json_string)
+
+?>
