ceda5343152117a3b35798253b2ec1515a10e20d
franedsandi <franedsandi@gmail.com>
2023-10-30 21:15:03 +0100

    [feat] complete task and delete task (only comple)

--- script.js
+++ script.js
@@ -1,32 +1,50 @@
 const { createApp } = Vue;
 
-createApp ({
-  data(){
-    return{
-      apiUrl:'server.php',
+createApp({
+  data() {
+    return {
+      apiUrl: 'server.php',
       title: 'To Do List',
-      list:[],
-      newTodo: '',
-    }
+      list: [],
+      newTodo: ''
+    };
   },
-  methods:{
-    getlist(){
+  methods: {
+    getList() {
       axios.get(this.apiUrl)
-        .then (result => {
+        .then(result => {
           this.list = result.data;
-        })
+        });
     },
-    addTask(){
+    addTask() {
       const data = new FormData();
-      data.append('todoItem', this.newTodo)
+      data.append('todoItem', this.newTodo);
       axios.post(this.apiUrl, data)
-      .then(result =>{
-        this.list = result.data;
-        this.newTodo= '';
-      })
+        .then(result => {
+          this.list = result.data;
+          this.newTodo = '';
+        });
+    },
+    removeTask(index) {
+      const data = new FormData();
+      data.append('indexToDelete', index);
+      axios.post(this.apiUrl, data)
+        .then(result => {
+          this.list = result.data;
+        });
+    },
+    toggleTask(index) {
+      this.list[index].completed = !this.list[index].completed;
+      const data = new FormData();
+      data.append('indexToUpdate', index);
+      data.append('completed', this.list[index].completed.toString());
+      axios.post(this.apiUrl, data)
+        .then(result => {
+          this.list = result.data;
+        });
     }
   },
-   mounted(){
-    this.getlist()
-   }
-}).mount('#app')
+  mounted() {
+    this.getList();
+  }
+}).mount('#app');
--- server.php
+++ server.php
@@ -2,16 +2,30 @@
 
 $json_string = file_get_contents('todo-list.json');
 
-$list = json_decode($json_string);
+$list = json_decode($json_string, true);
+
 if(isset($_POST['todoItem'])){
   $newItem = $_POST['todoItem'];
-
-  $list[] = $newItem;
+  $list[] = array("task" => $newItem, "completed" => false);
   file_put_contents('todo-list.json', json_encode($list));
-};
+}
+
+if(isset($_POST['indexToDelete'])){
+  $indexToDelete = $_POST['indexToDelete'];
+  if ($list[$indexToDelete]['completed'] === true) {
+    array_splice($list, $indexToDelete, 1);
+    file_put_contents('todo-list.json', json_encode($list));
+  }
+}
+
+if (isset($_POST['indexToUpdate']) && isset($_POST['completed'])) {
+  $indexToUpdate = $_POST['indexToUpdate'];
+  $completed = $_POST['completed'] === 'true' ? true : false;
+  $list[$indexToUpdate]['completed'] = $completed;
+  file_put_contents('todo-list.json', json_encode($list));
+}
 
 header('Content-Type: application/json');
-
 echo json_encode($list);
 
 ?>
